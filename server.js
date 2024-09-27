056b3156c477294e44d1bd44e775a9e834ed63ee
Gjhkyio <147998329+Gjhkyio@users.noreply.github.com>
2024-09-26 21:52:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -347,39 +347,29 @@ wss.on('connection', function (ws, req) {
 
 console.log('Listening on port:', wsPort);
 
-const audioApi = RtAudioApi.WINDOWS_WASAPI;
-const audioDeviceId = getDeviceId(rtAudio.getDevices(), "Virtual Audio Input (VB-Audio Virtual Cable)");
-const streamConfig = {
-  deviceId: audioDeviceId,
-  nChannels: 2,
-  firstChannel: 0
-};
+const rtAudio = new RtAudio(RtAudioApi.WINDOWS_WASAPI);
+const devices = rtAudio.getDevices();
+const deviceId = devices.map((e, index) => ({ name: e.name.trim(), index })).find(device => device.name === "Virtual Audio Input (VB-Audio Virtual Cable)").index;
 
-const audioFormat = RtAudioFormat.RTAUDIO_SINT16;
-const sampleRate = 22050;
-const frameSize = 50; // in milliseconds
-const frameBytes = frameSize * sampleRate / 1000;
-
-const rtAudio = new RtAudio(audioApi);
-rtAudio.openStream(null, streamConfig, audioFormat, sampleRate, frameBytes, "webvm", processAudioFrame, null, RtAudioStreamFlags.RTAUDIO_MINIMIZE_LATENCY, startStream);
-
-function getDeviceId(devices, deviceName) {
-  return devices.map(device => device.name.trim()).indexOf(deviceName);
-}
-
-function processAudioFrame(pcm) {
-  if (!pcm.every(p => p === 0x00)) {
-    wss.clients.forEach(client => {
-      client.send(pcm, { binary: true });
+rtAudio.openStream(
+  deviceId,
+  {
+    nChannels: 2,
+    firstChannel: 0
+  },
+  RtAudioFormat.RTAUDIO_SINT16,
+  22050,
+  250 * 22050 / 1000, // Frame size is 50ms
+  "webvm",
+  pcm => {
+    if (!pcm.every(p => p == 0x00)) wss.clients.forEach(c => {
+      c.send(pcm, { binary: true });
     });
-  }
-}
-
-function startStream() {
-  if (!rtAudio.isStreamRunning()) {
-    rtAudio.start();
-  }
-	}
+  },
+  null,
+  RtAudioStreamFlags.RTAUDIO_MINIMIZE_LATENCY,
+  e => { if (!rtAudio.isStreamRunning()) rtAudio.start(); }
+);
 
 rtAudio.start();
 
